df1df9d6c8fbe87cb18ef234f1f8480701cd5bbc
Ricardo Korzekwa <korzekwa.ricardo@gmail.com>
2025-02-02 20:16:00 -0300

    Ajusta configuração do banco de dados

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,16 @@
-from app import create_app
+from app import create_app, db
+from app.models import User
 
 app = create_app()
+
+# Criar as tabelas e usuário admin na primeira execução
+with app.app_context():
+    db.create_all()
+    if not User.query.filter_by(username='admin').first():
+        user = User(username='admin', is_admin=True)
+        user.set_password('admin123')  # Mude esta senha!
+        db.session.add(user)
+        db.session.commit()
 
 if __name__ == "__main__":
     app.run() 
